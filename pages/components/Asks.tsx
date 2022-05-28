6c63197fd88eea6d760a3bd13ff6a062c0337d5b
Samuel Diaz <samuel.diaz@meltstudio.co>
2022-05-28 09:41:36 -0500

    add pages and components

---
+++ pages/components/Asks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Asks() {
+  return <div>Asks</div>;
+}
+
+export default Asks;
---
+++ pages/payments.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Payments() {
+  return <div>payments</div>;
+}
+
+export default Payments;
